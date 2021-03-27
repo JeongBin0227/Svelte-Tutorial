660bc913ac5ba93b08532482307374b77d3d84ed
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-03-27 23:56:19 +0900

    feat: svelte-lifecycle (onmount,ondestory)

---
+++ svelte-lifecycle/src/Something.svelte
@@ -0,0 +1,26 @@
+<script>
+    import {onDestroy, onMount } from 'svelte'
+
+    // const h1 = document.querySelector('h1')
+    // console.log(h1.innerHTML)
+
+
+    // onMount(() => {
+    //     console.log('Mounted!')
+    // })
+    
+
+    onMount(() => {
+        console.log('Mounted!')
+        const h1 = document.querySelector('h1')
+        console.log(h1.innerHTML)
+    })
+
+    onDestroy(() => {
+        console.log('Destroy!')
+        const h1 = document.querySelector('h1')
+        console.log(h1.innerHTML)
+    })
+</script>
+
+<h1>Something..</h1>
